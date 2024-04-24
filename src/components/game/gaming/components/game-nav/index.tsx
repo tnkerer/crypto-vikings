da4ea6affbad57b739708231da2ea2d9e006afb3
JaumDark <viniciuspkfr@gmail.com>
2024-04-24 13:06:26 -0300

    feat: add game page and gameNav component

---
+++ src/components/game/gaming/components/game-nav/index.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import React, { useState } from 'react'
+
+import styles from './styles.module.scss'
+import Image from 'next/image'
+
+type Props = {}
+
+const GameNav = (props: Props) => {
+  const [navigation, setNavigation] = useState('battle')
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        {navigation === 'battle' ? <div>Warriors: 5</div> : null}
+        {navigation === 'runes' ? <div>Runes: 5</div> : null}
+        {navigation === 'dragons' ? <div>Dragons: 5</div> : null}
+        {navigation === 'staking' ? <div></div> : null}
+        {navigation === 'shop' ? <div></div> : null}
+      </div>
+
+      <div className={styles.navigationContainer}>
+        <div onClick={() => setNavigation('battle')} className={navigation == 'battle' ? styles.active : styles.option}>
+          Battle
+        </div>
+
+        <div onClick={() => setNavigation('runes')} className={navigation == 'runes' ? styles.active : styles.option}>
+          Runes
+        </div>
+
+        <div onClick={() => setNavigation('dragons')} className={navigation == 'dragons' ? styles.active : styles.option}>
+          Dragons
+        </div>
+
+        <div onClick={() => setNavigation('staking')} className={navigation == 'staking' ? styles.active : styles.option}>
+          Staking
+        </div>
+
+        <div onClick={() => setNavigation('shop')} className={navigation == 'shop' ? styles.active : styles.option}>
+          Shop
+        </div>
+      </div>
+
+      <div className={styles.filterContainer}>
+        {navigation == 'shop' ? null : 
+          <div className={styles.filter}>
+            Filter <Image src={'/assets/icons/downarrow.svg'} width={15} height={15} alt='Arrow' />
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default GameNav
--- src/components/game/gaming/index.tsx
+++ src/components/game/gaming/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
 import styles from './styles.module.scss'
+import GameNav from './components/game-nav'
 
 type Props = {}
 
 const GamingComponent = (props: Props) => {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.banner} />
+
+      <div className={styles.gameNav}>
+        <GameNav />
+      </div>
+
+      <div className={styles.contentContainer}>
+        {
+
+        }
+      </div>
     </div>
   )
 }
